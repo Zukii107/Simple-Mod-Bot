0c55126c7a740ccc3e1eee73e1c7e43c7b058ddd
Zukiii <73161979+Zukii107@users.noreply.github.com>
2020-10-24 19:27:50 +0700

    Add Mute and Unmute

---
+++ commands/unmute.js
@@ -0,0 +1,42 @@
+exports.run = async(client, message, args, guildConf, userConf) => {
+      if (!message.member.hasPermission("MANAGE_ROLES")) {
+        return message.channel.send({
+            embed: {
+                description: '<:error:742644211668484115> **You dont have \`MANAGE_ROLES\` permission**'
+            }
+        });
+      }
+  
+      if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+        return message.channel.send("I do not have permission to manage roles.");
+      }
+  
+      const user = message.mentions.members.first();
+  
+      if (!user) {
+        return message.channel.send(
+          "Please mention the member to who you want to unmute"
+        );
+      }
+      
+      let muterole = message.guild.roles.cache.find(x => x.name === "muted") //from database that make muted roles
+      
+      
+   if(user.roles.cache.has(muterole)) {
+        return message.channel.send("Given User do not have mute role so what i am suppose to take")
+      }
+      
+      
+      user.roles.remove(muterole)
+      
+      await message.channel.send(`**${message.mentions.users.first().username}** is unmuted`)
+      
+      user.send(`You are now unmuted from **${message.guild.name}**`)
+
+  };
+  module.exports.help = {
+    name: "unmute",
+    description: "Unmute member",
+    dm: false,
+    aliases: []
+}
